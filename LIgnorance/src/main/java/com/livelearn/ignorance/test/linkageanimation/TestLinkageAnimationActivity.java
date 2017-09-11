dfa9c1c3680b7d394727f869beb40c0f3ce464cc
FPhoenixCorneaE <13104871646@163.com>
2017-09-11 12:24:04 +0800

    解决联动动画滑动不流畅；

--- LIgnorance/src/main/java/com/livelearn/ignorance/test/linkageanimation/TestLinkageAnimationActivity.java
+++ LIgnorance/src/main/java/com/livelearn/ignorance/test/linkageanimation/TestLinkageAnimationActivity.java
@@ -7,9 +7,7 @@ import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,8 +15,9 @@ import android.widget.TextView;
 import com.livelearn.ignorance.R;
 import com.livelearn.ignorance.base.BaseActivity;
 import com.livelearn.ignorance.widget.smarttablayout.SmartTabLayout;
+import com.nineoldandroids.view.ViewHelper;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -37,15 +36,20 @@ public class TestLinkageAnimationActivity extends BaseActivity {
     @BindView(R.id.rl_root)
     RelativeLayout rlRoot;
 
-    private ImageView imageView1, imageView2, imageView3;
-    private int pagerWidth;
-    private int pagerHeight;
-    private int[] colors;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    //存放显示内容的View
+    private List<Integer> mViews = Arrays.asList(
+            R.layout.page_test_linkage_animation_01,
+            R.layout.page_test_linkage_animation_02,
+            R.layout.page_test_linkage_animation_03
+    );
+    //背景颜色
+    private int[] colors = new int[]{
+            Color.parseColor("#6c2da2"),
+            Color.parseColor("#1fb7dc"),
+            Color.parseColor("#ff9000")
+    };
+    private ViewPagerAdapter viewPagerAdapter;
+    private int pagerWidth, pagerHeight;
 
     @Override
     public int getLayoutResource() {
@@ -53,27 +57,11 @@ public class TestLinkageAnimationActivity extends BaseActivity {
     }
 
     public void initLayout(Bundle savedInstanceState) {
-        //存放显示内容的View
-        ArrayList<View> mViews = new ArrayList<>();
-        //背景颜色
-        colors = new int[]{Color.parseColor("#6c2da2"), Color.parseColor("#1fb7dc"), Color.parseColor("#ff9000")};
-
-        View layout1 = View.inflate(mContext, R.layout.page_test_linkage_animation_01, null);
-        imageView1 = (ImageView) layout1.findViewById(R.id.iv_imageView1);
-
-        View layout2 = View.inflate(mContext, R.layout.page_test_linkage_animation_02, null);
-        imageView2 = (ImageView) layout2.findViewById(R.id.iv_imageView2);
-
-        View layout3 = View.inflate(mContext, R.layout.page_test_linkage_animation_03, null);
-        imageView3 = (ImageView) layout3.findViewById(R.id.iv_imageView3);
-
-        mViews.add(layout1);
-        mViews.add(layout2);
-        mViews.add(layout3);
-
-        //ViewPager设置适配器
+        //限定预加载页面个数
         vpViewpager.setOffscreenPageLimit(mViews.size());
-        vpViewpager.setAdapter(new ViewPagerAdapter(mViews));
+        //ViewPager设置适配器
+        viewPagerAdapter = new ViewPagerAdapter(mContext, mViews);
+        vpViewpager.setAdapter(viewPagerAdapter);
         stlTab.setViewPager(vpViewpager);
 
         vpViewpager.post(new Runnable() {
@@ -87,93 +75,100 @@ public class TestLinkageAnimationActivity extends BaseActivity {
         vpViewpager.setBackgroundColor(colors[0]);
     }
 
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        private ImageView imageView1, imageView2, imageView3;
+        private boolean exchange01, exchange12;
+        private float lastPositionOffset1, lastPositionOffset2, translationY1, translationY3, translationY5;
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            if (pagerWidth == 0) {
+                return;
+            }
+            if (imageView1 == null) {
+                imageView1 = (ImageView) viewPagerAdapter.getPage(0).findViewById(R.id.iv_imageView1);
+            }
+            if (imageView2 == null) {
+                imageView2 = (ImageView) viewPagerAdapter.getPage(1).findViewById(R.id.iv_imageView2);
+            }
+            if (imageView3 == null) {
+                imageView3 = (ImageView) viewPagerAdapter.getPage(2).findViewById(R.id.iv_imageView3);
+            }
+            if (0 < positionOffset) {//滑动百分比趋近于1的时候，会突变为0
+                if (position == 0) {
+                    translationY1 += imageView1.getHeight() * (positionOffset - lastPositionOffset1);
+                    //为了防止用户手指按住页面持续快速滑动，图片飞出屏幕之外，加以限制
+                    if (translationY1 < 0) {
+                        translationY1 = 0;
+                    }
+                    if (translationY1 > imageView1.getHeight()) {
+                        translationY1 = imageView1.getHeight();
+                    }
+                    ViewHelper.setTranslationY(imageView1, translationY1);
+                    translationY3 -= imageView2.getHeight() * (positionOffset - lastPositionOffset1);
+                    if (translationY3 > 0) {
+                        translationY3 = 0;
+                    }
+                    if (translationY3 < -imageView2.getHeight()) {
+                        translationY3 = -imageView2.getHeight();
+                    }
+                    ViewHelper.setTranslationY(imageView2, translationY3);
+                    exchange01 = true;
+                }
+                if (position == 1) {
+                    translationY3 += imageView2.getHeight() * (positionOffset - lastPositionOffset2);
+                    if (translationY3 < 0) {
+                        translationY3 = 0;
+                    }
+                    if (translationY3 > imageView2.getHeight()) {
+                        translationY3 = imageView2.getHeight();
+                    }
+                    ViewHelper.setTranslationY(imageView2, translationY3 - imageView2.getHeight());
+                    translationY5 -= imageView3.getHeight() * (positionOffset - lastPositionOffset2);
+                    if (translationY5 > 0) {
+                        translationY5 = 0;
+                    }
+                    if (translationY5 < -imageView3.getHeight()) {
+                        translationY5 = -imageView3.getHeight();
+                    }
+                    ViewHelper.setTranslationY(imageView3, translationY5);
+                    exchange12 = true;
+                }
+            }
+            if (exchange01) {
+                lastPositionOffset1 = positionOffset;
+            }
+            if (exchange12) {
+                lastPositionOffset2 = positionOffset;
+            }
+            Drawable backgroundDrawable = getDrawable(position, positionOffset);
+            vpViewpager.setBackground(backgroundDrawable);
+            rlRoot.setBackground(backgroundDrawable);
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+            if (i == 0) {//i==0 什么都没做; i==1 正在滑动; i==2 滑动完毕了
+                exchange01 = false;
+                exchange12 = false;
+                lastPositionOffset1 = 0;
+                lastPositionOffset2 = 0;
+                translationY1 = 0;
+                translationY3 = 0;
+                translationY5 = 0;
+            }
+        }
+    };
+
     /**
      * UI事件监听
      */
     public void setListeners() {
-        vpViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            boolean exchange01;
-            boolean exchange12;
-            private float lastPositionOffset1;
-            private float lastPositionOffset2;
-            private float translationY1 = 0;
-            private float translationY3 = 0;
-            private float translationY5 = 0;
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if (pagerWidth == 0) {
-                    return;
-                }
-                if (0 < positionOffset) {//滑动百分比趋近于1的时候，会突变为0
-                    if (position == 0) {
-                        translationY1 += imageView1.getHeight() * (positionOffset - lastPositionOffset1);
-                        //为了防止用户手指按住页面持续快速滑动，图片飞出屏幕之外，加以限制
-                        if (translationY1 < 0) {
-                            translationY1 = 0;
-                        }
-                        if (translationY1 > imageView1.getHeight()) {
-                            translationY1 = imageView1.getHeight();
-                        }
-                        imageView1.setTranslationY(translationY1);
-                        translationY3 -= imageView2.getHeight() * (positionOffset - lastPositionOffset1);
-                        if (translationY3 > 0) {
-                            translationY3 = 0;
-                        }
-                        if (translationY3 < -imageView2.getHeight()) {
-                            translationY3 = -imageView2.getHeight();
-                        }
-                        imageView2.setTranslationY(translationY3);
-                        exchange01 = true;
-                    }
-                    if (position == 1) {
-                        translationY3 += imageView2.getHeight() * (positionOffset - lastPositionOffset2);
-                        if (translationY3 < 0) {
-                            translationY3 = 0;
-                        }
-                        if (translationY3 > imageView2.getHeight()) {
-                            translationY3 = imageView2.getHeight();
-                        }
-                        imageView2.setTranslationY(translationY3 - imageView2.getHeight());
-                        translationY5 -= imageView3.getHeight() * (positionOffset - lastPositionOffset2);
-                        if (translationY5 > 0) {
-                            translationY5 = 0;
-                        }
-                        if (translationY5 < -imageView3.getHeight()) {
-                            translationY5 = -imageView3.getHeight();
-                        }
-                        imageView3.setTranslationY(translationY5);
-                        exchange12 = true;
-                    }
-                }
-                if (exchange01) {
-                    lastPositionOffset1 = positionOffset;
-                }
-                if (exchange12) {
-                    lastPositionOffset2 = positionOffset;
-                }
-                vpViewpager.setBackground(getDrawable(position, positionOffset));
-                rlRoot.setBackground(getDrawable(position, positionOffset));
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-                if (i == 0) {//i==0 什么都没做; i==1 正在滑动; i==2 滑动完毕了
-                    exchange01 = false;
-                    exchange12 = false;
-                    lastPositionOffset1 = 0;
-                    lastPositionOffset2 = 0;
-                    translationY1 = 0;
-                    translationY3 = 0;
-                    translationY5 = 0;
-                }
-            }
-        });
+        vpViewpager.addOnPageChangeListener(mOnPageChangeListener);
     }
 
     protected Drawable getDrawable(int position, float positionOffset) {
@@ -191,50 +186,5 @@ public class TestLinkageAnimationActivity extends BaseActivity {
         canvas.drawPaint(paint);
 
         return new BitmapDrawable(getResources(), bitmap);
-    }
-
-    public class ViewPagerAdapter extends PagerAdapter {
-        // 界面列表
-        private List<View> views;
-
-        ViewPagerAdapter(List<View> mViews) {
-            this.views = mViews;
-        }
-
-        /**
-         * 获得当前界面数
-         */
-        @Override
-        public int getCount() {
-            if (views != null) {
-                return views.size();
-            }
-            return 0;
-        }
-
-        /**
-         * 初始化position位置的界面
-         */
-        @Override
-        public Object instantiateItem(View view, int position) {
-            ((ViewPager) view).addView(views.get(position), 0);
-            return views.get(position);
-        }
-
-        /**
-         * 判断是否由对象生成界面
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object arg1) {
-            return (view == arg1);
-        }
-
-        /**
-         * 销毁position位置的界面
-         */
-        @Override
-        public void destroyItem(View view, int position, Object arg2) {
-            ((ViewPager) view).removeView(views.get(position));
-        }
     }
 }
---
+++ LIgnorance/src/main/java/com/livelearn/ignorance/test/linkageanimation/ViewPagerAdapter.java
@@ -0,0 +1,70 @@
+package com.livelearn.ignorance.test.linkageanimation;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.util.SparseArrayCompat;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+/**
+ * Created by wkz on 2017/09/11.
+ */
+
+public class ViewPagerAdapter extends PagerAdapter {
+    // 界面列表
+    private final List<Integer> views;
+    private final SparseArrayCompat<WeakReference<View>> holder;
+    private final LayoutInflater inflater;
+
+    ViewPagerAdapter(Context context, @NonNull List<Integer> mViews) {
+        this.views = mViews;
+        this.holder = new SparseArrayCompat<>(mViews.size());
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    /**
+     * 获得当前界面数
+     */
+    @Override
+    public int getCount() {
+        return views.size();
+    }
+
+    /**
+     * 初始化position位置的界面
+     */
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = inflater.inflate(views.get(position), container, false);
+        container.addView(view);
+        holder.put(position, new WeakReference<>(view));
+        return view;
+    }
+
+    /**
+     * 判断是否由对象生成界面
+     */
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return object == view;
+    }
+
+    /**
+     * 销毁position位置的界面
+     */
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        holder.remove(position);
+        container.removeView((View) object);
+    }
+
+    public View getPage(int position) {
+        final WeakReference<View> weakRefItem = holder.get(position);
+        return weakRefItem != null ? weakRefItem.get() : null;
+    }
+}
